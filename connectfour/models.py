d07300cab147a83638c140fb398c428dd2542847
Gonzalo Mena-Mendoza <gonzalo@mena.com.mx>
2023-10-24 21:35:02 -0600

    Add endpoint to create a new game
    ```bash
    $ curl http://localhost:8080/connectfour/ && echo
    ```
    
    TODO
    - Require POST instead of GET.
    - Make traling slash optional.

--- connectfour/models.py
+++ connectfour/models.py
@@ -3,4 +3,12 @@ from django.db import models
 class GameState(models.Model):
     next_player = models.IntegerField(default=1)
     winner = models.IntegerField(default=None, null=True)
-    board = models.CharField(max_length=200)
+    board = models.JSONField(max_length=200)
+
+    def to_json(self):
+        return {
+            "id": self.id,
+            "next_player": self.next_player,
+            "winner": self.winner,
+            "board": self.board,
+        }
--- connectfour/views.py
+++ connectfour/views.py
@@ -1,4 +1,17 @@
-from django.http import HttpResponse
+from django.http import JsonResponse
+from .models import GameState
+
+WIN_COUNT = 4
+ROW_COUNT = 6
+COLUMN_COUNT = 7
 
 def index(request):
-    return HttpResponse("¡Hola!")
+    gamestate = GameState(
+        next_player=1,
+        winner=None,
+        board=[[0] * COLUMN_COUNT] * ROW_COUNT,
+    )
+
+    gamestate.save()
+
+    return JsonResponse(gamestate.to_json(), safe=False)
